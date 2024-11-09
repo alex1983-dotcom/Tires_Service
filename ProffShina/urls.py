9e05eefd78b2e019a5d0edae38a939a0893f4027
Макаров Александр <alebarda2018@gmail.com>
2024-11-09 21:25:09 +0300

    Измнения сохранены

--- ProffShina/urls.py
+++ ProffShina/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views as auth_views
 from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
-from user_registration import views
+from user_registration import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Админка
@@ -28,9 +28,9 @@ urlpatterns = [
     path('user/', include('user_registration.urls')),  # Маршруты приложения user_registration
     path('api/', include('user_registration.urls_api')),  # Маршруты API
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),  # Медиа-файлы
-    path('login/', views.login_view, name='login'),  # Логин
+    path('login/', user_views.login_view, name='login'),  # Логин
     path('logout/', auth_views.LogoutView.as_view(next_page='home_page'), name='logout'),  # Логаут
-    path('personal_cabinet/', views.personal_cabinet, name='personal_cabinet'),  # Личный кабинет
+    path('personal_cabinet/', user_views.personal_cabinet, name='personal_cabinet'),  # Личный кабинет
     path('', include('content.urls')),  # Главная страница
 ]
 
