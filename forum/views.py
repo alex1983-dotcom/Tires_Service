ae130d7aa7dad33ee81b13b77b7b51fc0e4e2842
Макаров Александр <alebarda2018@gmail.com>
2024-11-21 13:16:35 +0300

    Изменения в views.py

--- forum/views.py
+++ forum/views.py
@@ -1,30 +1,21 @@
-from django.shortcuts import render, get_object_or_404
 from django.views import View
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView
-from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Category, Thread, Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HomePageView(View):
-    """
-    Представление для главной страницы форума.
-    """
     template_name = 'forum/home_page_forum.html'
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
 
-class CategoryListView(ListView):
-    """
-    Представление для отображения списка категорий на форуме.
-    """
+class CategoryListView(LoginRequiredMixin, ListView):
     model = Category
     template_name = 'forum/category_list.html'
     context_object_name = 'categories'
 
-class ThreadListView(View):
-    """
-    Представление для отображения списка тем в категории.
-    """
+class ThreadListView(LoginRequiredMixin, View):
     template_name = 'forum/thread_list.html'
 
     def get(self, request, category_id, *args, **kwargs):
@@ -33,10 +24,7 @@ class ThreadListView(View):
         context = {'category': category, 'threads': threads}
         return render(request, self.template_name, context)
 
-class PostListView(View):
-    """
-    Представление для отображения списка сообщений в теме.
-    """
+class PostListView(LoginRequiredMixin, View):
     template_name = 'forum/post_list.html'
 
     def get(self, request, thread_id, *args, **kwargs):
@@ -44,3 +32,10 @@ class PostListView(View):
         posts = Post.objects.filter(thread=thread)
         context = {'thread': thread, 'posts': posts}
         return render(request, self.template_name, context)
+
+    def post(self, request, thread_id, *args, **kwargs):
+        thread = get_object_or_404(Thread, id=thread_id)
+        content = request.POST.get('content')
+        if content:
+            Post.objects.create(thread=thread, created_by=request.user, message=content)
+        return redirect('post_list', thread_id=thread.id)
