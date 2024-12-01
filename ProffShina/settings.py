25daf98c70afb4fb9daa7ee2915db7fa6c374f65
Макаров Александр <alebarda2018@gmail.com>
2024-12-01 23:51:24 +0300

    Поместил секретный ключ в файл .env

--- ProffShina/settings.py
+++ ProffShina/settings.py
@@ -20,8 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&b^mr*4(^q8s*j^kr&29h+prhf9(5&k9ro@g!%!ic*8m$(-q=r'
+
+from dotenv import load_dotenv
+
+load_dotenv()  # Загружает переменные окружения из файла .env
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
